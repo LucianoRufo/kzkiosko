0f0b177a178db0070e62ab9ef357b9b9aa835f19
Ignacio Picun <ignaciopicun@protonmail.com>
2019-09-20 01:22:08 -0300

    Added sign in button

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,23 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { AsyncStorage } from "react-native";
 import { NavigationActions } from "react-navigation";
 import * as Google from "expo-google-app-auth";
+import { Button } from "react-native-ui-kitten";
 import * as firebase from "firebase";
 import authState from "../states/authState";
 import Container from "../components/Container";
 import authConfig from "../authConfig";
 
 export default function SignIn({ navigation }) {
-  useEffect(() => {
+  signIn();
+
+  return (
+    <Container>
+      <Button onPress={signIn}>Sign In</Button>
+    </Container>
+  );
+
+  function signIn() {
     signInAsync().catch(err => {
       authState.clear();
       AsyncStorage.setItem("@KzKiosko:accessToken", JSON.stringify(null));
       AsyncStorage.setItem("@KzKiosko:user", JSON.stringify(null));
       console.log(err);
     });
-  });
-
-  return <Container></Container>;
+  }
 
   async function signInAsync() {
     const unsubscribe = firebase.auth().onAuthStateChanged(async user => {
@@ -32,7 +39,6 @@ export default function SignIn({ navigation }) {
         const { type, accessToken, idToken, user } = await Google.logInAsync(
           authConfig
         );
-        console.log("TYPE", type);
         if (type === "success") {
           const credential = firebase.auth.GoogleAuthProvider.credential(
             idToken
